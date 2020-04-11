08556e5189dab5faf9f12b1f320676b40ecbfc94
mifl <mifl.pc@gmail.com>
2020-04-11 20:48:37 +0200

    rootdir: drop init.bq.check.sh

--- rootdir/Android.mk
+++ rootdir/Android.mk
@@ -239,14 +239,6 @@ LOCAL_MODULE_PATH  := $(TARGET_ROOT_OUT)
 include $(BUILD_PREBUILT)
 
 include $(CLEAR_VARS)
-LOCAL_MODULE       := init.bq.check.sh
-LOCAL_MODULE_TAGS  := optional eng
-LOCAL_MODULE_CLASS := ETC
-LOCAL_SRC_FILES    := etc/init.bq.check.sh
-LOCAL_MODULE_PATH  := $(TARGET_OUT_ETC)
-include $(BUILD_PREBUILT)
-
-include $(CLEAR_VARS)
 LOCAL_MODULE       := init.bq.tp.sh
 LOCAL_MODULE_TAGS  := optional eng
 LOCAL_MODULE_CLASS := ETC
--- rootdir/etc/init.bq.check.sh
+++
@@ -1,5 +0,0 @@
-if [ -f /cache/bq_reset ]; then
-	setprop persist.bq.reset 1
-else
-	setprop persist.bq.reset 0
-fi
