ca56381c2661efe0fd2e9be75170b355dd475ca5
mifl <mifl.pc@gmail.com>
2020-04-12 08:50:13 +0200

    rootdir: disable init.qcom.modem_links.sh
    Change-Id: I9037239b9d2a46a900de7d46e5f75cb3e0bced6c

--- rootdir/Android.mk
+++ rootdir/Android.mk
@@ -196,13 +196,13 @@ LOCAL_SRC_FILES    := etc/init.carrier.rc
 LOCAL_MODULE_PATH  := $(TARGET_ROOT_OUT)
 include $(BUILD_PREBUILT)
 
-include $(CLEAR_VARS)
-LOCAL_MODULE       := init.qcom.modem_links.sh
-LOCAL_MODULE_TAGS  := optional eng
-LOCAL_MODULE_CLASS := ETC
-LOCAL_SRC_FILES    := etc/init.qcom.modem_links.sh
-LOCAL_MODULE_PATH  := $(TARGET_OUT_ETC)
-include $(BUILD_PREBUILT)
+#include $(CLEAR_VARS)
+#LOCAL_MODULE       := init.qcom.modem_links.sh
+#LOCAL_MODULE_TAGS  := optional eng
+#LOCAL_MODULE_CLASS := ETC
+#LOCAL_SRC_FILES    := etc/init.qcom.modem_links.sh
+#LOCAL_MODULE_PATH  := $(TARGET_OUT_ETC)
+#include $(BUILD_PREBUILT)
 
 # vendor files that live under device/bq/msm8937-common/rootdir/etc/
 
