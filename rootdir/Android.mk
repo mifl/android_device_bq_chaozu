b77688e5201e0185bab93619568613cb587b8b80
mifl <mifl.pc@gmail.com>
2020-04-12 09:09:02 +0200

    rootdir: disable init.carrier.rc
    Change-Id: I96b2b89200c30b7713b03130d5b4b0966bff9ab0

--- rootdir/Android.mk
+++ rootdir/Android.mk
@@ -188,13 +188,13 @@ include $(BUILD_PREBUILT)
 #LOCAL_MODULE_PATH  := $(TARGET_OUT_VENDOR_EXECUTABLES)
 #include $(BUILD_PREBUILT)
 
-include $(CLEAR_VARS)
-LOCAL_MODULE       := init.carrier.rc
-LOCAL_MODULE_TAGS  := optional eng
-LOCAL_MODULE_CLASS := ETC
-LOCAL_SRC_FILES    := etc/init.carrier.rc
-LOCAL_MODULE_PATH  := $(TARGET_ROOT_OUT)
-include $(BUILD_PREBUILT)
+#include $(CLEAR_VARS)
+#LOCAL_MODULE       := init.carrier.rc
+#LOCAL_MODULE_TAGS  := optional eng
+#LOCAL_MODULE_CLASS := ETC
+#LOCAL_SRC_FILES    := etc/init.carrier.rc
+#LOCAL_MODULE_PATH  := $(TARGET_ROOT_OUT)
+#include $(BUILD_PREBUILT)
 
 #include $(CLEAR_VARS)
 #LOCAL_MODULE       := init.qcom.modem_links.sh
