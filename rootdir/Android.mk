3b22b4f3d0c091b390b07dceba046f478f7735b2
mifl <mifl.pc@gmail.com>
2020-04-12 09:42:02 +0200

    rootdir: disable init.qcom.coex.sh
    Change-Id: Iee381d0f52b0f33433614fd58ef9bbe5465c86b3

--- rootdir/Android.mk
+++ rootdir/Android.mk
@@ -43,13 +43,13 @@ LOCAL_SRC_FILES    := etc/init.qcom.efs.sync.sh
 LOCAL_MODULE_PATH  := $(TARGET_OUT_VENDOR_EXECUTABLES)
 include $(BUILD_PREBUILT)
 
-include $(CLEAR_VARS)
-LOCAL_MODULE       := init.qcom.coex.sh
-LOCAL_MODULE_TAGS  := optional eng
-LOCAL_MODULE_CLASS := ETC
-LOCAL_SRC_FILES    := etc/init.qcom.coex.sh
-LOCAL_MODULE_PATH  := $(TARGET_OUT_VENDOR_EXECUTABLES)
-include $(BUILD_PREBUILT)
+#include $(CLEAR_VARS)
+#LOCAL_MODULE       := init.qcom.coex.sh
+#LOCAL_MODULE_TAGS  := optional eng
+#LOCAL_MODULE_CLASS := ETC
+#LOCAL_SRC_FILES    := etc/init.qcom.coex.sh
+#LOCAL_MODULE_PATH  := $(TARGET_OUT_VENDOR_EXECUTABLES)
+#include $(BUILD_PREBUILT)
 
 #include $(CLEAR_VARS)
 #LOCAL_MODULE       := init.qcom.sdio.sh
